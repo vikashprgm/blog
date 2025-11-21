3b2954e0d9e6c07133313ecbbfa30e40832ef989
vikashprgm <vikash.prgm@gmail.com>
2025-11-22 00:16:31 +0530

    Shorten Code

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,17 +6,6 @@ import {
   TabsList,
   TabsTrigger,
 } from '@/components/animate-ui/components/animate/tabs';
-import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import { Dashboard } from '@/components/dashboard';
 import { getCurrentUserFn } from '@/utils/auth';
 import { LoginForm } from '@/components/login-form';
